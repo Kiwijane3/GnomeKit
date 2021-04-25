42c69ec3e9ff78430347b4655bf5b9ed1e67c9d8
Jane Fraser <janef0421@icloud.com>
2021-04-26 10:20:09 +1200

    Added main(_:) to allow for immediate processing on main loop

--- Sources/GtkKit/Idle.swift
+++ Sources/GtkKit/Idle.swift
@@ -12,3 +12,18 @@ public func idle(_ handler: @escaping() -> Bool) {
 		Unmanaged<TimerClosureHolder>.fromOpaque(holderPointer!).release();
 	}
 }
+
+public func main(_ handler: @escaping() -> Void) {
+	let action = { () -> Bool in
+		handler()
+		return false
+	}
+	let holder = TimerClosureHolder(action)
+	let opaque = Unmanaged<TimerClosureHolder>.passRetained(holder).toOpaque()
+	idleAddFull(priority: Int(G_PRIORITY_HIGH), function: { (holderPointer) -> gboolean in
+		let holder = Unmanaged<TimerClosureHolder>.fromOpaque(holderPointer!).takeUnretainedValue()
+		return holder.call() ? 1 : 0
+	}, data: opaque, notify: { (holderPointer) in
+		Unmanaged<TimerClosureHolder>.fromOpaque(holderPointer!).release()
+	})
+}
