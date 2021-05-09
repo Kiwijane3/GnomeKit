f28f1a88564bd8702879ebb108c4b1309b3b43ed
Jane Fraser <janef0421@icloud.com>
2021-05-09 15:58:06 +1200

    Increased robustness of isDarkTheme()

--- Sources/GtkKit/DarkTheme.swift
+++ Sources/GtkKit/DarkTheme.swift
@@ -3,6 +3,9 @@ import Gtk
 
 public func isDarkTheme() -> Bool {
 	let settings = Settings.getDefault()
-	let themeName = settings?.get(property: .gtkThemeName).getString()
-	return themeName?.contains("dark") ?? false
+	let themeName = settings?.get(property: .gtkThemeName)
+	if themeName?.ptr == nil {
+		return false
+	}
+	return themeName?.getString().contains("dark") ?? false
 }
